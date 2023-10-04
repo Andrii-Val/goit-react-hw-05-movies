430ae7b187c601e4eee30b26fc07ac6d96058716
Andrii-Val <andrii.val1006@gmail.com>
2023-10-04 14:19:25 +0300

    18

--- src/index.js
+++ src/index.js
@@ -7,9 +7,8 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename='/goit-react-hw-05-movies'>
+    {/* <BrowserRouter> */}
       <App />
-      
-      <GlobalStyle/>
     </BrowserRouter>
   </React.StrictMode>
 );
